3a29b7fb6e4799497ff1c2ac2b2763c21e40ec60
vinwoonki <vinwoonki98@gmail.com>
2026-01-05 09:11:42 +0900

    chore : 테스트

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,10 +1,53 @@
-
-import { View } from 'react-native';
-
-
+import { StyleSheet, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function HomeScreen() {
   return (
-   <View></View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.topBoxContainer}>
+        <Box />
+        <Box />
+        <Box />
+      </View>
+      <View style={styles.middleBoxContainer}>
+        <Box />
+        <Box />
+        <Box />
+      </View>
+      <View style={styles.bottomBoxContainer}>
+        <Box />
+        <Box />
+        <Box />
+      </View>
+    </SafeAreaView>
   );
 }
+
+function Box() {
+  return <View style={styles.box} />;
+}
+
+const styles = StyleSheet.create({
+  container: {
+    justifyContent: "space-between",
+    flex: 1,
+  },
+  topBoxContainer: {
+    flexDirection: "row",
+    justifyContent: "flex-end",
+    gap: 5,
+  },
+  middleBoxContainer: {
+    flexDirection: "row",
+    justifyContent: "space-evenly",
+  },
+  bottomBoxContainer: {
+    flexDirection: "row",
+    gap: 5,
+  },
+  box: {
+    width: 50,
+    height: 50,
+    backgroundColor: "red",
+  },
+});
